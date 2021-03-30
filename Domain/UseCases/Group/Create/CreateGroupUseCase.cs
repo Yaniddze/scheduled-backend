4bf00c32155594cdb3eb4d25dff1356e55c29794
Zinevich Yan <facecrack1337@gmail.com>
2021-03-30 20:50:24 +0500

    :fire: Add unique group name

--- Domain/UseCases/Group/Create/CreateGroupUseCase.cs
+++ Domain/UseCases/Group/Create/CreateGroupUseCase.cs
@@ -5,6 +5,8 @@ using Domain.Abstractions.Mediator;
 using Domain.Abstractions.Outputs;
 using Domain.Services.Identity;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Domain.Abstractions.Outputs.ActionOutput;
 
 namespace Domain.UseCases.Group.Create
 {
@@ -12,24 +14,37 @@ namespace Domain.UseCases.Group.Create
     {
         private readonly IAppContext _context;
         private readonly IMediator _mediator;
+        private readonly IUnitOfWorkCreator _unitOfWork;
 
-        public CreateGroupUseCase(IAppContext context, IMediator mediator)
+        public CreateGroupUseCase(IAppContext context, IMediator mediator, IUnitOfWorkCreator unitOfWork)
         {
             _context = context;
             _mediator = mediator;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task<IOutput> Handle(CreateGroupInput request, CancellationToken cancellationToken)
         {
             var currentUser = await _mediator.Send(new GetCurrentUserInput(), cancellationToken);
+
+            using var unit = _unitOfWork.CreateUnitOfWork();
+
+            var exist = await _context.Groups.AnyAsync(x => x.Name == request.Name, cancellationToken);
+
+            if (exist)
+            {
+                return Failure("Такая группа уже существует");
+            }
             
             var group = new Entities.Group(request.Name, currentUser);
 
             _context.Groups.Add(group);
 
             await _context.SaveChangesAsync(cancellationToken);
+
+            await unit.Apply();
             
-            return ActionOutput.SuccessData(new
+            return SuccessData(new
             {
                 group.Id,
             });
