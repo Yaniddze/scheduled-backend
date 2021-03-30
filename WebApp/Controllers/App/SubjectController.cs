d3d8de94e317eb21504459e1d6073b91befb314f
Zinevich Yan <facecrack1337@gmail.com>
2021-03-31 03:54:18 +0500

    :wrench: Minor fixes

--- WebApp/Controllers/App/SubjectController.cs
+++ WebApp/Controllers/App/SubjectController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Threading.Tasks;
+using Domain.Enums;
 using Domain.UseCases.Subject.Get;
 using Microsoft.AspNetCore.Mvc;
+using Schedule.Identity;
 using Schedule.Implementations;
 
 namespace Schedule.Controllers.App
 {
-    [Route("api/v1/subject")]
+    [Route("api/v1/subject"), AuthorizeByRoles(UserRole.Participant)]
     public class SubjectController: Controller
     {
         private readonly IUseCaseDispatcher _dispatcher;
