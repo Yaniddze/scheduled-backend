8c01166022fb0c6935b5351758646c351a79a4a6
Zinevich Yan <facecrack1337@gmail.com>
2021-03-30 22:01:46 +0500

    :wrench: Add scheduled controller

---
+++ WebApp/Controllers/App/ScheduledController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Domain.Enums;
+using Domain.UseCases.Schedule.Create;
+using Domain.UseCases.Schedule.Delete;
+using Microsoft.AspNetCore.Mvc;
+using Schedule.Identity;
+using Schedule.Implementations;
+
+namespace Schedule.Controllers.App
+{
+    [Route("api/v1/scheduled"), AuthorizeByRoles(UserRole.Participant)]
+    public class ScheduledController: Controller
+    {
+        private readonly IUseCaseDispatcher _dispatcher;
+
+        public ScheduledController(IUseCaseDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+        
+        [HttpPut]
+        public async Task<IActionResult> Create([FromBody] CreateScheduleInput request) => 
+            await _dispatcher.DispatchAsync(request);
+        
+        [HttpDelete]
+        public async Task<IActionResult> Remove([FromBody] DeleteScheduleInput request) => 
+            await _dispatcher.DispatchAsync(request);
+    }
+}
