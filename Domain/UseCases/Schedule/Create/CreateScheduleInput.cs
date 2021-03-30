3096cdb44fff116d71495e877ba6f3444ed72765
Zinevich Yan <facecrack1337@gmail.com>
2021-03-30 21:14:41 +0500

    :alien: Create scheduled report use case

---
+++ Domain/UseCases/Schedule/Create/CreateScheduleInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Domain.Abstractions.Mediator;
+using Domain.Extensions;
+using FluentValidation;
+
+namespace Domain.UseCases.Schedule.Create
+{
+    public class CreateScheduleInput: IUseCaseInput
+    {
+        public int GroupId { get; set; }
+        public DateTime StartDate { get; set; }
+        public int DurationInMinutes { get; set; }
+        public int SubjectId { get; set; }
+    }
+
+    public class CreateScheduleInputValidator : AbstractValidator<CreateScheduleInput>
+    {
+        public CreateScheduleInputValidator()
+        {
+            RuleFor(x => x.GroupId)
+                .EntityReference();
+
+            RuleFor(x => x.StartDate)
+                .NotEmpty();
+
+            RuleFor(x => x.SubjectId)
+                .EntityReference();
+
+            RuleFor(x => x.DurationInMinutes)
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ Domain/UseCases/Schedule/Create/CreateScheduleUseCase.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Domain.Abstractions.Data;
+using Domain.Abstractions.Mediator;
+using Domain.Abstractions.Outputs;
+using Domain.Entities;
+using Domain.Services.Identity;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Domain.Abstractions.Outputs.ActionOutput;
+
+namespace Domain.UseCases.Schedule.Create
+{
+    public class CreateScheduleUseCase: IUseCase<CreateScheduleInput>
+    {
+        private readonly IAppContext _context;
+        private readonly IMediator _mediator;
+
+        public CreateScheduleUseCase(IAppContext context, IMediator mediator)
+        {
+            _context = context;
+            _mediator = mediator;
+        }
+
+        public async Task<IOutput> Handle(CreateScheduleInput request, CancellationToken cancellationToken)
+        {
+            var currentUser = await _mediator.Send(new GetCurrentUserInput(), cancellationToken);
+
+            var group = await _context.Groups
+                .Include(x => x.GroupSubjects)
+                .FirstOrDefaultAsync(x => x.Id == request.GroupId, cancellationToken);
+
+            if (group is null)
+            {
+                return Failure("Группа не найдена");
+            }
+
+            if (group.OwnerId != currentUser.Id)
+            {
+                return Failure("Вы не имеете прав для редактирования этой группы");
+            }
+            
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(x => x.Id == request.SubjectId, cancellationToken);
+
+            if (subject is null)
+            {
+                return Failure("Предмет не найден");
+            }
+            
+            var groupSubject = new GroupSubject
+            {
+                DurationInMinutes = request.DurationInMinutes,
+                StartDate = request.StartDate,
+                
+                Group = group,
+                GroupId = group.Id,
+                
+                Subject = subject,
+                SubjectId = subject.Id,
+            };
+
+            _context.GroupSubjects.Add(groupSubject);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return SuccessData(new
+            {
+                groupSubject.Id,
+            });
+        }
+    }
+}
